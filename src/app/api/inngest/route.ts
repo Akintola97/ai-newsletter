4e3833008aaac99389529d5d432ea77fdd12a23d
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-09-10 07:53:56 -0500

    update

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,15 +1,32 @@
+// import { serve } from "inngest/next";
+// import { inngest } from "../../../inngest/client";
+// import newsletter from "@/inngest/news";
+// import scheduler from "@/inngest/scheduler";
+
+
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   signingKey: process.env.INNGEST_SIGNING_KEY,
+//   functions: [
+//   newsletter, scheduler
+//     /* your functions will be passed here later! */
+//   ],
+// });
+
+
+
+
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+import { inngest } from "@/inngest/client";
 import newsletter from "@/inngest/news";
 import scheduler from "@/inngest/scheduler";
 
-
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   signingKey: process.env.INNGEST_SIGNING_KEY,
   functions: [
-  newsletter, scheduler
-    /* your functions will be passed here later! */
+    newsletter,   // ✅ only one function id now
+    scheduler,
   ],
 });
